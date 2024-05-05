ad96cb3690386bce3fe7e8d9e9a329625cf95876
Kunal Verma <127933656+Kunalverma45@users.noreply.github.com>
2024-05-05 19:54:52 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,7 @@ dotenv.config({ path: "./config/config.env" });
 
 app.use(
   cors({
-    origin: ["https://mern-project-api-kunalverma45s-projects.vercel.app/"],
+    origin: ["https://mern-project-api-kunalverma45s-projects.vercel.app"],
     methods: ["POST"],
     credentials: true,
   })
